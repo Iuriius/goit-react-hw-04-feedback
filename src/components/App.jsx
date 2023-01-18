2f190766863c50429c5b8b39db5ca591139986f0
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-01-18 18:04:59 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 import Statistics from './Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setCounterGood] = useState(0);
+  const [neutral, setCounterNeutral] = useState(0);
+  const [bad, setCounterBad] = useState(0);
+  const [total, setCounterTotal] = useState(0);
+  const [positivePercentage, setCounterPositivePercentage] = useState(0);
+
+  const stateMap = {
+    good: setCounterGood,
+    neutral: setCounterNeutral,
+    bad: setCounterBad,
   };
 
-  onLeaveFeedback = event => {
+  const onUpdateFeedback = event => {
     const { name } = event.target;
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    stateMap[name](prevState => prevState + 1);
   };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    return Math.round((good * 100) / totalFeedback) + '%';
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const isEmptyList = !(good + neutral + bad);
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
+  useEffect(() => {
+    setCounterTotal(good + neutral + bad);
+  }, [bad, good, neutral]);
+
+  useEffect(() => {
+    setCounterPositivePercentage(Math.round((good * 100) / total) + '%');
+  }, [good, total]);
+
+  const isEmptyList = !(good + neutral + bad);
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(stateMap)}
+          onLeaveFeedback={onUpdateFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {isEmptyList ? (
+          <Notification message="There is no feedback given 😞" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {isEmptyList ? (
-            <Notification message="There is no feedback given 😞" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
 }
